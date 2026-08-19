80a896ba0b53aa4decf1bf2ab7b585a6ead17f66
Oleksandr Yavorskyi <slimnation2@gmail.com>
2026-08-19 20:18:41 +0200

    let's go

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/svitlana-yavorska/',
+  base: '/UGC-Creator/',
   plugins: [
     tailwindcss(),
   ],
